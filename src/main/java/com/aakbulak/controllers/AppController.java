00b9a30b82b6530564df0347fb8828e6eb625e74
alper.akbulak <alper.akbulak@indbilisim.com.tr>
2017-10-17 11:47:34 +0300

    Controller

---
+++ src/main/java/com/aakbulak/controllers/AppController.java
@@ -0,0 +1,24 @@
+package com.aakbulak.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @author Alper AKBULAK<alper.akbulak@indbilisim.com.tr>
+ * @since 0.0.16
+ */
+@Controller
+public class AppController {
+    @RequestMapping("/")
+    String home(ModelMap modal){
+        modal.addAttribute("title","CRUD Example");
+        return "index";
+    }
+
+    @RequestMapping("/partials/{page}")
+    String partialHandler(@PathVariable("page") final String page) {
+        return page;
+    }
+}
